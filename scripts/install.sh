37e124ab6779e388415800c8fd4ce90b2bf109aa
Axel Kummer <kummeraxel@gmail.com>
2017-12-29 07:39:08 +0100

    Setup logbook demo stack

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd $( dirname "${BASH_SOURCE[0]}" )
+
+set -ex
+
+cd ..
+
+composer install --ignore-platform-reqs
+
+docker-compose down -v
+docker-compose build
+docker-compose up -d
+
+sleep 30
+
+docker-compose exec db sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /var/data/database-dump.sql'
